7ddfe9acb467816991f434b93ae6284dd2d4f369
glenbuck503 <glenbuck503@gmail.com>
2020-10-26 23:54:28 -0700

    add user input to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story").show();
-
+  $("#favthings").submit(function(event) {
     event.preventDefault();
+
+    let color = $("input#color").val();
+    let sport = $("input#sport").val();
+    let city= $("input#city").val();
+    let number = $("input#number").val();
+    let car = $("input#car").val();
+    let movie= $("input#movie").val();
+
+    let arrayAnswers =[];
+
+    arrayAnswers.push(color,sport,city,number,car,movie);
+
+
+
+
+    
+    $(".fav").append("<li>" + arrayAnswers[1] + "</li>");
+    $(".fav").append("<li>" + arrayAnswers[0] + "</li>");
+    $(".fav").append("<li>" + arrayAnswers[3] + "</li>");
+		$(".fav").show();
+     
+    
   });
 });
+
+
